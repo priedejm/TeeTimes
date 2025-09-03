d74e57354272d64257177dd8962f29bd20ba5d16
jpriede <jpriede@insiderealestate.com>
2025-09-02 22:37:07 -0400

    refarcot edits

--- muniScraper.py
+++ muniScraper.py
@@ -55,6 +55,7 @@ def scrape_tee_times(dayOfWeek):
     print(f"[INFO] Navigating to: {url}")
 
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
